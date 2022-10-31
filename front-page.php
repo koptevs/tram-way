48cbe6c2321d09b304fef9522fa09569426ac21b
Igors Koptevs <igors.koptevs@gmail.com>
2022-10-31 15:42:47 +0200

    Reformat code

--- front-page.php
+++ front-page.php
@@ -16,42 +16,43 @@ endif; ?>
 
 	<div class="wrapper" id="index-wrapper">
 
-<!--		<div class="--><?php
-//		echo esc_attr( $container ); ?><!--" id="content" tabindex="-1">-->
-<!---->
-<!--			<div class="row">-->
+		<!--		<div class="--><?php
+		//		echo esc_attr( $container ); ?><!--" id="content" tabindex="-1">-->
+		<!---->
+		<!--			<div class="row">-->
 
-				<main class="site-main" id="main">
+		<main class="site-main" id="main">
 
-					<div class="d-flex align-items-center justify-content-center"
-						 style="height: 200px; background-color: #ffca68; color: #000000;">
-						<p class="fw-bold fs-1">SECTION</p>
-					</div>
-					<div class="d-flex align-items-center justify-content-center"
-						 style="height: 200px; background-color: #006778; color: #fff5f5;">
-						<p class="fw-bold fs-1">SECTION</p>
-					</div><div class="d-flex align-items-center justify-content-center"
-							   style="height: 200px; background-color: #bc4c4c; color: #ffffff;">
-						<p class="fw-bold fs-1">SECTION</p>
-					</div>
+			<div class="d-flex align-items-center justify-content-center"
+				 style="height: 200px; background-color: #ffca68; color: #000000;">
+				<p class="fw-bold fs-1">SECTION</p>
+			</div>
+			<div class="d-flex align-items-center justify-content-center"
+				 style="height: 200px; background-color: #006778; color: #fff5f5;">
+				<p class="fw-bold fs-1">SECTION</p>
+			</div>
+			<div class="d-flex align-items-center justify-content-center"
+				 style="height: 200px; background-color: #bc4c4c; color: #ffffff;">
+				<p class="fw-bold fs-1">SECTION</p>
+			</div>
 
-				</main><!-- #main -->
+		</main><!-- #main -->
 
-				<!-- The pagination component -->
-				<!--                -->
-				<?php
-				// understrap_pagination();
-				?>
+		<!-- The pagination component -->
+		<!--                -->
+		<?php
+		// understrap_pagination();
+		?>
 
-				<!-- Do the right sidebar check -->
-				<!--                -->
-				<?php
-				// get_template_part( 'global-templates/right-sidebar-check' );
-				?>
+		<!-- Do the right sidebar check -->
+		<!--                -->
+		<?php
+		// get_template_part( 'global-templates/right-sidebar-check' );
+		?>
 
-<!--			</div><!-- .row -->-->
-<!---->
-<!--		</div><!-- #content -->-->
+		<!--			</div><!-- .row -->-->
+		<!---->
+		<!--		</div><!-- #content -->-->
 
 	</div><!-- #index-wrapper -->
 
